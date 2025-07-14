fd004dd9344f23cd85510de00cbf0dd267afdf1f
lstue <lstuebchen@gmail.com>
2025-07-14 20:08:06 +0200

    14 July 2025 - Now show the saved image when the user views their meal

--- app/src/main/java/com/example/nomlymealtracker/helper/Helper.kt
+++ app/src/main/java/com/example/nomlymealtracker/helper/Helper.kt
@@ -2,9 +2,12 @@ package com.example.nomlymealtracker.helper
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.text.format.DateUtils
 import android.util.Base64
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
 import com.example.nomlymealtracker.data.models.Meal
 import com.google.firebase.Timestamp
 import java.text.SimpleDateFormat
@@ -35,7 +38,7 @@ object Helper
     // A function used to share meal details with someone on another app
     fun shareMeal(context: Context, meal: Meal) {
         val shareText = buildString {
-            appendLine("Check out this meal!")
+            appendLine("Check out this meal!\n")
             appendLine("Title: ${meal.title}")
             appendLine("Description: ${meal.description}")
             appendLine("Type: ${meal.type}")
@@ -44,7 +47,7 @@ object Helper
             appendLine("Carbs: ${meal.carbs?.toString() ?: "0"}g")
             appendLine("Fats: ${meal.fats?.toString() ?: "0"}g")
             appendLine("Calories: ${meal.calories?.toString() ?: "N/A"}")
-            appendLine("Consumed at: ${meal.timestamp.toDate()}")
+            appendLine("Consumed at: ${ formatTimestamp(meal.timestamp) }")
         }
 
         val intent = Intent(Intent.ACTION_SEND).apply {
@@ -67,5 +70,16 @@ object Helper
         }
 
         return Base64.encodeToString(bytes, Base64.NO_WRAP)
+    }
+
+    fun decodeBase64ToImageBitmap(base64String: String): ImageBitmap? {
+        return try {
+            val decodedBytes = Base64.decode(base64String, Base64.DEFAULT)
+            val bitmap = BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.size)
+            bitmap?.asImageBitmap()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
     }
 }
--- app/src/main/java/com/example/nomlymealtracker/ui/screens/viewMeal/ViewMealScreen.kt
+++ app/src/main/java/com/example/nomlymealtracker/ui/screens/viewMeal/ViewMealScreen.kt
@@ -1,57 +1,33 @@
 package com.example.nomlymealtracker.ui.screens.viewMeal
 
-import android.view.View
+import android.graphics.Bitmap
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListState
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Image
-import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.Share
-import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExtendedFloatingActionButton
-import androidx.compose.material3.FilterChip
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberModalBottomSheetState
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.unit.dp
 import com.example.nomlymealtracker.ui.theme.NomlyMealTrackerTheme
-import kotlinx.coroutines.launch
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SheetState
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.TopAppBar
@@ -63,21 +39,24 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.DefaultLifecycleObserver
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
-import com.example.nomlymealtracker.data.models.Meal
 import com.example.nomlymealtracker.data.models.MealType
 import com.example.nomlymealtracker.helper.Helper
 import com.example.nomlymealtracker.helper.Helper.shareMeal
-import com.example.nomlymealtracker.ui.screens.home.HomeViewModel
-import com.example.nomlymealtracker.ui.screens.home.mealCard.MealCard
 import com.example.nomlymealtracker.ui.theme.MidOrange
 import com.google.firebase.Timestamp
 import kotlinx.coroutines.CoroutineScope
+import android.graphics.BitmapFactory
+import android.util.Base64
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.material3.Card
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import coil.compose.rememberAsyncImagePainter
+import com.example.nomlymealtracker.helper.Helper.decodeBase64ToImageBitmap
 
 // Preview for the ViewMealScreen
 @Preview
@@ -87,15 +66,17 @@ fun ViewMealScreenContentPreview(){
         ViewMealScreenContent(
             snackbarHost = SnackbarHostState(),
 
+            image = null,
             title = "Double Cheese Burger",
             description = "A really long explanation of this burger and what its made of. Surprice, its mostly beef and some onions. Yum",
-            timeOfConsumption = Timestamp.now(),
+            timeOfConsumption = "12:32pm",
             selectedMealType = MealType.BREAKFAST,
             portionSize = "1x",
             protein = "60",
             carbs = "39",
             fats = "12",
             calories = "387",
+            timestamp = Timestamp.now(),
 
             onShareClick = {},
             onBackClick = {}
@@ -122,6 +103,10 @@ fun ViewMealScreen(
 
     val context = LocalContext.current
 
+    val imageBitmap = remember(meal?.imageBase64) {
+        meal?.imageBase64?.let { decodeBase64ToImageBitmap(it) }
+    }
+
     // A box used to have a loader visible while the item loads
     Box(modifier = Modifier.fillMaxSize()) {
         if (meal == null) {
@@ -130,15 +115,17 @@ fun ViewMealScreen(
             ViewMealScreenContent(
                 snackbarHost = snackbarHost,
 
+                image = imageBitmap,
                 title = meal!!.title,
                 description = meal!!.description,
-                timeOfConsumption = meal!!.timestamp,
+                timeOfConsumption = meal!!.timeEaten,
                 selectedMealType = meal!!.type,
                 portionSize = meal!!.portionSize,
                 protein = meal!!.protein?.toString(),
                 carbs = meal!!.carbs?.toString(),
                 fats = meal!!.fats?.toString(),
                 calories = meal!!.calories?.toString(),
+                timestamp = meal!!.timestamp,
 
                 onShareClick = { meal?.let { shareMeal(context, it) } },
                 onBackClick = onBackClick
@@ -153,15 +140,17 @@ fun ViewMealScreen(
 fun ViewMealScreenContent(
     snackbarHost: SnackbarHostState,
 
+    image: ImageBitmap?,
     title: String,
     description: String,
-    timeOfConsumption: Timestamp,
+    timeOfConsumption: String,
     selectedMealType: MealType,
     portionSize: String,
     protein: String?,
     carbs: String?,
     fats: String?,
     calories: String?,
+    timestamp: Timestamp,
 
     onShareClick: () -> Unit,
     onBackClick: () -> Unit,
@@ -200,16 +189,29 @@ fun ViewMealScreenContent(
                     .background(MidOrange),
                 contentAlignment = Alignment.Center
             ) {
-                Icon(
-                    imageVector = Icons.Default.Image,
-                    contentDescription = "Meal Image",
-                    tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
-                )
-                Text(
-                    text = "Image Reference",
-                    style = MaterialTheme.typography.labelLarge,
-                    modifier = Modifier.padding(top = 48.dp)
-                )
+                if (image != null) {
+                    Image(
+                        bitmap = image,
+                        contentDescription = "Meal Image",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                } else {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                            Icon(
+                                imageVector = Icons.Default.Image,
+                                contentDescription = "Add Image",
+                                tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
+                            )
+                            Text(
+                                text = "No Image Available",
+                                style = MaterialTheme.typography.labelLarge,
+                                modifier = Modifier.padding(top = 8.dp)
+                            )
+                        }
+                    }
+                }
             }
 
             Spacer(modifier = Modifier.height(12.dp))
@@ -224,7 +226,7 @@ fun ViewMealScreenContent(
             Spacer(modifier = Modifier.height(12.dp))
 
             Text("Time Eaten", style = MaterialTheme.typography.titleMedium)
-            Text(Helper.formatTimestamp(timeOfConsumption) , style = MaterialTheme.typography.bodyLarge)
+            Text(timeOfConsumption , style = MaterialTheme.typography.bodyLarge)
 
             Spacer(modifier = Modifier.height(12.dp))
 
@@ -259,6 +261,11 @@ fun ViewMealScreenContent(
                     color = MaterialTheme.colorScheme.primary
                 )
             }
+
+            Spacer(modifier = Modifier.height(6.dp))
+
+            Text("Created", style = MaterialTheme.typography.titleMedium)
+            Text(Helper.formatTimestamp(timestamp), style = MaterialTheme.typography.bodyLarge)
         }
     }
 }
